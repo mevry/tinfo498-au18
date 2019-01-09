99d03627eb044f7dc4088cf984f567d2caf60fbf
Derek Miller <derekjmiller@gmail.com>
2019-01-08 18:54:00 -0800

    Fix syntax

--- scripts/lab3-add-testusers.ps1
+++ scripts/lab3-add-testusers.ps1
@@ -3,6 +3,6 @@ $passwords='farout','123','Bach','Dragon','Secretshazam','pirateplato','i!h@d@M$
 $index=1
 foreach ($password in $passwords){
     $secure = ConvertTo-SecureString $password -AsPlainText -Force
-    New-LocalUser -Name "User" + $index -Password $secure
+    New-LocalUser -Name User$index -Password $secure
     $index++
 }
